a710be7cf0d8635f5f19eba1548cd3502ae937f2
James Chapman <jammchap@ututm.onmicrosoft.com>
2020-08-27 13:39:06 -0500

    It greets me now, world.

--- GitHub Test/Program.cs
+++ GitHub Test/Program.cs
@@ -6,7 +6,7 @@ namespace GitHub_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello James!");
         }
     }
 }
